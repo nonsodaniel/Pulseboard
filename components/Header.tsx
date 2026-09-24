e6d0fbc05b5918722a30cb6c87f7c30e234d5170
Nonso Daniel <nonso@company.com>
2026-09-24 08:55:35 +0300

    bug: search bar removed from header (Bug 9)

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,16 +53,6 @@ export default function Header({ onMenuClick, breadcrumb }: HeaderProps) {
 
       {/* Right */}
       <div className="flex items-center gap-2">
-        {/* Search */}
-        <div className="relative hidden md:block">
-          <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
-          <input
-            type="text"
-            placeholder="Search..."
-            className="pl-9 pr-4 py-1.5 text-sm border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 w-56"
-          />
-        </div>
-
         {/* Notifications */}
         <div className="relative" ref={notifRef}>
           <button
